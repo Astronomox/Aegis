3ff6a8df5b19e30628b6c484aca421bd969b3ab8
ASTRONOMOX <abdullahioriola02@gmail.com>
2026-09-19 12:00:00 +0100

    Build out aegis-passenger/lib/audio.js (part 2/3)

--- aegis-passenger/lib/audio.js
+++ aegis-passenger/lib/audio.js
@@ -3,3 +3,8 @@ import { MOCK_MODE } from './supabase';
 
 /**
  * Request microphone permission. Call once on mount.
+ */
+export async function requestAudioPermission() {
+  if (MOCK_MODE) return true;
+  try {
+    const { granted } = await AudioModule.requestRecordingPermissionsAsync();
