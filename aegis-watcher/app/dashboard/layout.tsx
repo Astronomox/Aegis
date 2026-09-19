3ad35b482c65f2a4b16a3083d5a2877e93cb8b26
ASTRONOMOX <abdullahioriola02@gmail.com>
2026-09-19 12:00:00 +0100

    Build out aegis-watcher/app/dashboard/layout.tsx (part 1/3)

--- aegis-watcher/app/dashboard/layout.tsx
+++ aegis-watcher/app/dashboard/layout.tsx
@@ -1,11 +1,3 @@
 export default function DashboardLayout({
   children,
 }: {
-  children: React.ReactNode;
-}) {
-  return (
-    <div style={{ height: '100vh', width: '100vw', position: 'relative', overflow: 'hidden' }}>
-      {children}
-    </div>
-  );
-}
