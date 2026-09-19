c797fcc2c3f9ecb09b6c5bb5341944f055ba47b4
ASTRONOMOX <abdullahioriola02@gmail.com>
2026-09-19 12:00:00 +0100

    Build out aegis-passenger/lib/theme.js (part 2/3)

--- aegis-passenger/lib/theme.js
+++ aegis-passenger/lib/theme.js
@@ -6,3 +6,11 @@ export const COLORS = {
   white: '#F5F5F5',
   red: '#E63946',
   green: '#2A9D5C',
+};
+
+export const FONTS = {
+  // TODO on-site: swap to loaded custom fonts
+  display: 'System', // Aegis display font for wordmark only
+  body: 'System',    // Montserrat
+  bodyBold: 'System',
+};
